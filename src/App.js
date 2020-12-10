d045198d56040c49b84d35a1aeb1f62d42a09671
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-12-10 12:56:51 -0400

    salvando fotos

--- src/App.js
+++ src/App.js
@@ -5,30 +5,58 @@ class App extends React.Component {
   constructor() {
     super();
 
+    this.fetchDog = this.fetchDog.bind(this);
+    
+    this.renderDogElement = this.renderDogElement.bind(this);
+    this.savePicture = this.savePicture.bind(this);
+
     this.state = {
-      image: ''
+      dog: undefined,
+      storedDogs: [],
+      loading: true
     };
   }
 
   async fetchDog() {
+    this.setState({loading: true})
     const requestDog = await fetch('https://dog.ceo/api/breeds/image/random');
     const returnedObject = await requestDog.json();
     this.setState({
-      image: returnedObject.message
+      dog: returnedObject.message,
+      loading: false
     });
   }
 
   componentDidMount() {
     this.fetchDog();
   }
+
+  savePicture() {
+    this.setState(({ storedDogs, dog }) =>({
+      storedDogs: [...storedDogs, dog]
+    }));
+    this.fetchDog();
+  }
+
+  renderDogElement() {
+    return (
+      <div>
+        <img className="showPicture" src={this.state.dog} alt="dog" />
+        <button type="button" onClick={this.savePicture}>Salvar foto</button>
+      </div>
+    )  
+  }
   
   render() {
     const loading = <span className="showLoading">Loading...</span>;
-    const image = <img className="showPicture" src={this.state.image} alt='dog'/>;
+    const { storedDogs } = this.state;
 
     return (
       <div>
-        <p>{this.state.image === '' ? loading : image}</p>
+        <span>
+          {storedDogs.map((picture) => (<img src={picture} alt="dog" />))}
+        </span>
+        <p>{this.state.loading ? loading : this.renderDogElement()}</p>
       </div>
     );
   }
